9665750586851cd441a3df7e0a545c8ddd66f734
Ziyan <jaraidaniel@gmail.com>
2015-03-15 19:33:24 +0100

    Rename device

--- full_maguro.mk
+++ full_maguro.mk
@@ -34,6 +34,7 @@ $(call inherit-product, device/samsung/maguro/device.mk)
 # Set those variables here to overwrite the inherited values.
 PRODUCT_NAME := full_maguro
 PRODUCT_DEVICE := maguro
-PRODUCT_BRAND := Android
-PRODUCT_MODEL := AOSP on Maguro
+PRODUCT_BRAND := Google
+PRODUCT_MODEL := Galaxy Nexus
+PRODUCT_MANUFACTURER := Samsung
 PRODUCT_RESTRICT_VENDOR_FILES := false
